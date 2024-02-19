d9aedba05dbb62807650df2a51ef1d9f5141ee90
aishwarya1999-k <kalalaishwarya1999@gmail.com>
2024-02-19 23:31:11 +0530

    Use constructor

--- src/components/News.js
+++ src/components/News.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  constructor(){
+    super();
+    console.log("Hiii this is constructor ");
+  }
   render() {
-    
+
     return (
       <div className="container my-3">
         <h2>NewsMonkey - Top Headlines</h2>
         <div className="row">
           <div className="col-md-4">
-            <NewsItem title="myTitle" description="mydesc" />
+            <NewsItem title="myTitle" description="mydesc" imageUrl="https://ichef.bbci.co.uk/news/1024/branded_news/1544E/production/_132681178_gazawar.png" newsUrl="TODO"/>
           </div>
           <div className="col-md-4">
             <NewsItem title="myTitle" description="mydesc" />
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+  
   render() {
-    let {title,description}=this.props;
+    let {title,description,imageUrl,newsUrl}=this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src="https://ichef.bbci.co.uk/news/1024/branded_news/1544E/production/_132681178_gazawar.png" className="card-img-top" alt="..."/>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsItem" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsdetail/" className="btn btn-sm btn-primary">Read more</a>
           </div>
         </div>
       </div>
